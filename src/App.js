860c25cfd1b9e284706afb3bc99ca229ca9fcd84
occludedarp <cmrnmr@gmail.com>
2019-12-10 09:03:26 -0500

    completed handler function stretch

--- src/App.js
+++ src/App.js
@@ -14,9 +14,26 @@ let [homeName, updateHomeName] = useState('Lions');
 
 let [awayName, updateAwayName] = useState('Tigers');
 
+const homeTouchdown = () => {
+  updateHomeScore(homeScoreIs + 7)
+}
+
+const homeFieldgoal = () => {
+  updateHomeScore(homeScoreIs + 3)
+}
+
+const awayTouchdown = () => {
+  updateAwayScore(awayScoreIs + 7)
+
+}
+const awayFieldgoal = () => {
+  updateAwayScore(awayScoreIs + 3)
+}
+
   return (
     <div className="container">
       <section className="scoreboard">
+
         <div className="topRow">
           <HomeTeam homeScore={homeScoreIs} homeName={homeName}/>
           <Timer />
@@ -24,19 +41,21 @@ let [awayName, updateAwayName] = useState('Tigers');
         </div>
 
         <BottomRow />
+        
       </section>
 
       <section className="buttons">
+
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown" onClick={() => updateHomeScore(homeScoreIs + 7)}>Home Touchdown</button>
-          <button className="homeButtons__fieldGoal" onClick={() => updateHomeScore(homeScoreIs + 3)}>Home Field Goal</button>
-        </div>
-        <div className="awayButtons">
-          <button className="awayButtons__touchdown" onClick={() => updateAwayScore(awayScoreIs + 7)}>Away Touchdown</button>
-          <button className="awayButtons__fieldGoal" onClick={() => updateAwayScore(awayScoreIs + 3)}>Away Field Goal</button>
+          <button className="homeButtons__touchdown" onClick={homeTouchdown}>Home Touchdown</button>
+          <button className="homeButtons__fieldGoal" onClick={homeFieldgoal}>Home Field Goal</button>
         </div>
 
+        <div className="awayButtons">
+          <button className="awayButtons__touchdown" onClick={awayTouchdown}>Away Touchdown</button>
+          <button className="awayButtons__fieldGoal" onClick={awayFieldgoal}>Away Field Goal</button>
+        </div>
 
       </section>
     </div>
